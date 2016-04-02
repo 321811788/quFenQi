283d2fd4fbf90d06a0124cc1ad521501327bf358
ltx <ltx@163.com>
2016-04-02 22:17:39 +0800

    初始化项目

---
+++ quFenQi/quFenQi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  quFenQi
+//
+//  Created by 戒了。 on 16/4/2.
+//  Copyright © 2016年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ quFenQi/quFenQi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  quFenQi
+//
+//  Created by 戒了。 on 16/4/2.
+//  Copyright © 2016年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
